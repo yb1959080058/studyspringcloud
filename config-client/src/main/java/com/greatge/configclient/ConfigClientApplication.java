b9566bc1fc94b239db011088683257211181bb71
于博 <1959080058@qq.com>
2019-04-15 17:55:01 +0800

    修改配置文件

--- config-client/src/main/java/com/greatge/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/greatge/configclient/ConfigClientApplication.java
@@ -1,13 +1,24 @@
 package com.greatge.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${hello}")
+    String hello;
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return hello;
     }
 
 }
